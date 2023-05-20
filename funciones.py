0b7643fbdcdf6d9099412d719b4ddeee0d5feea5
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-05-20 10:12:44 -0500

    Se actualizo archivo funciones

--- funciones.py
+++ funciones.py
@@ -15,25 +15,37 @@ def multiplicar(n1, n2):
     #Retorno del dato.
     return multiplicar
 
+def promedio(cantidad):
+    for i in range(cantidad):
+        numero = float(input("Ingrese un número: "))
+        numeros.append(numero)
+        
+    suma = sum(numeros)
+    promedio = suma / cantidad
+
+    return promedio
 
 #Llamado o invocación del metodo.
 
-n1 = int(input("Ingrese el primer número: "))
-n2 = int(input("Ingrese el segundo número: "))
-
 print("Menu de opciones:")
-op = input("Ingrese la oopción segun la operación a realizar: \n1. Suma. \n2. Restar. \n3. Multipicación. \n4. División.\n")
+op = input("Ingrese la oopción segun la operación a realizar: \n1. Suma. \n2. Restar. \n3. Multipicación. \n4. División.\n5. Promedio. \n")
 
 if op == "1":
     #Definición o invocaión del metodo sumar.
+    n1 = int(input("Ingrese el primer número: "))
+    n2 = int(input("Ingrese el segundo número: "))
     sumar()
 
 elif op == "2":
     #Definición o invocaión del metodo con parametros.
+    n1 = int(input("Ingrese el primer número: "))
+    n2 = int(input("Ingrese el segundo número: "))
     restar(n1,n2)
     
 
 elif op == "3":
+    n1 = int(input("Ingrese el primer número: "))
+    n2 = int(input("Ingrese el segundo número: "))
     #Definición o invocaión del metodo con valor de retorno.
     #multiplicar(n1,n2)
     #Forma de usar el metodo con valor de retorno directamente en una impreción.
@@ -45,3 +57,12 @@ elif op == "3":
         print("El producto es menor que 50.")
     else:
         print("El producto es mayor que 50.")
+
+elif op == "4":
+    print("No hay nada :'v")
+
+elif op == "5":
+    cantidad = int(input("Ingrese la cantidad de números: "))
+    numeros = []
+
+    print(f"El promedio de los números es: {promedio(cantidad)}")
