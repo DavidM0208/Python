5828624b62ba5581df9d11ebbe293e39078dff80
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-05-21 13:26:13 -0500

    Se hicieron arreglos en el codigo de cara y sellazo

--- carisellazo.py
+++ carisellazo.py
@@ -1,27 +1,21 @@
 from random import randint
-
 saldo = 5000
 op = 1
-
 def lanzarMoneda():
     nombre = input("Ingrese su nombre: ")
     print(f"Bienvenido {nombre} al juego cara y sellazo.")
     moneda = randint(1,2)
     return moneda
-
 def ganar(saldo, apuesta):
     print("Usted ganó.")
     resultado = saldo + apuesta * 2
     print(f"Su nuevo saldo es de {resultado}")
-
 def perder(saldo, apuesta):
     print("Usted perdió.")
     resultado = saldo - apuesta
     print(f"Su nuevo saldo es de {resultado}")
-
 def jugar():
     moneda = lanzarMoneda()
-
     eleccion = int(input("Digite 1 para escoger cara o 2 para escoger sello.\n"))
     if eleccion == 1:
         print("Usted elegio cara.")
@@ -41,7 +35,6 @@ def jugar():
     elif moneda==2 and eleccion==1:
         perder(saldo, apuesta)
         print("Salio sello, usted escogio cara, Perdiste!...")
-
 while op == 1:
     jugar()
     op = int(input("Digite 1 si quiere jugar de nuevo.\nDigite 2 si quiere salir.\n"))
