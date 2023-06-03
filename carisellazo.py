6a4fbf79f319ea601d8b90e073c315720bbdee41
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-06-03 07:37:58 -0500

    Actualizacion de datos

--- carisellazo.py
+++ carisellazo.py
@@ -4,25 +4,28 @@ op = 1
 def lanzarMoneda():
     nombre = input("Ingrese su nombre: ")
     print(f"Bienvenido {nombre} al juego cara y sellazo.")
+
     moneda = randint(1,2)
     return moneda
 def ganar(saldo, apuesta):
     print("Usted ganó.")
     resultado = saldo + apuesta * 2
     print(f"Su nuevo saldo es de {resultado}")
+
 def perder(saldo, apuesta):
     print("Usted perdió.")
     resultado = saldo - apuesta
     print(f"Su nuevo saldo es de {resultado}")
+
 def jugar():
     moneda = lanzarMoneda()
     eleccion = int(input("Digite 1 para escoger cara o 2 para escoger sello.\n"))
+    
     if eleccion == 1:
         print("Usted elegio cara.")
     elif eleccion == 2:
         print("Usted elegio sello.")
     apuesta = int(input("¿Cuanto desea apostar?\n"))
-
     if moneda==1 and eleccion==1:
         ganar(saldo, apuesta)
         print("Salio cara, usted escogio cara, Ganaste!...")
